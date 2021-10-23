b5516da6881316d8a665b4377bf6a871ccd54fcd
GENG Minghong <58876849+GENGMinghong@users.noreply.github.com>
2021-10-23 13:40:20 +0800

    Update Models.py

--- classification/Models.py
+++ classification/Models.py
@@ -35,3 +35,17 @@ class Resnext50(nn.Module):
 
     def forward(self, x):
         return self.sigm(self.base_model(x))
+
+class Resnext50_old(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        resnet = models.resnext50_32x4d(pretrained=True)
+        resnet.fc = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(in_features=resnet.fc.in_features, out_features=n_classes)
+        )
+        self.base_model = resnet
+        self.sigm = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigm(self.base_model(x))
