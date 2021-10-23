43e88f0c44ee74e9e91cff2ccec71b5dae043b3b
GENG Minghong <58876849+GENGMinghong@users.noreply.github.com>
2021-10-23 16:24:29 +0800

    Update Models.py

--- classification/Models.py
+++ classification/Models.py
@@ -21,6 +21,20 @@ from dataloader.DataLoader import data_transforms
 from dataloader.DataLoader import Load_Image_Information_Train
 from dataloader.DataLoader import Load_Image_Information_Test
 
+class DenseNet(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        resnet = models.resnext50_32x4d(pretrained=True)
+        resnet.fc = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(in_features=resnet.fc.in_features, out_features=512),
+            nn.LeakyReLU(0.1),
+            nn.Linear(512, n_classes)
+        )
+        self.base_model = resnet
+        self.sigm = nn.Sigmoid()
+
+
 # TODO: Add FC layer for a different number of classes (103) and a Sigmoid instead of a default Softmax.
 class Resnext50(nn.Module):
     def __init__(self, n_classes):
@@ -28,13 +42,17 @@ class Resnext50(nn.Module):
         resnet = models.resnext50_32x4d(pretrained=True)
         resnet.fc = nn.Sequential(
             nn.Dropout(p=0.2),
-            nn.Linear(in_features=resnet.fc.in_features, out_features=n_classes)
+            nn.Linear(in_features=resnet.fc.in_features, out_features=512),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(p=0.3),
+            nn.Linear(512, n_classes)
         )
         self.base_model = resnet
         self.sigm = nn.Sigmoid()
 
     def forward(self, x):
         return self.sigm(self.base_model(x))
+
 
 class Resnext50_old(nn.Module):
     def __init__(self, n_classes):
@@ -49,3 +67,34 @@ class Resnext50_old(nn.Module):
 
     def forward(self, x):
         return self.sigm(self.base_model(x))
+
+
+
+class ResNet50(nn.Module):
+    def __init__(self, num_outputs):
+        # resnet_cls.load_state_dict(torch.load(resnet_weights_path))
+        super(ResNet50, self).__init__()
+        resnet_cls = models.resnet50(pretrained=True)
+        self.resnet = resnet_cls
+        layer4 = self.resnet.layer4
+        self.resnet.layer4 = nn.Sequential(
+            nn.Dropout(0.5),
+            layer4
+        )
+        self.resnet.avgpool = AvgPool()
+        self.resnet.fc = nn.Linear(2048, num_outputs)
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+        for param in self.resnet.layer4.parameters():
+            param.requires_grad = True
+        for param in self.resnet.fc.parameters():
+            param.requires_grad = True
+
+    def forward(self, x):
+        out = self.resnet(x)
+        return out
+
+class AvgPool(nn.Module):
+    def forward(self, x):
+        return F.avg_pool2d(x, x.shape[2:])
+
