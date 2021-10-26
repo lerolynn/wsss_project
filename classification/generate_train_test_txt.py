b1d0c479d74e5c0011457990dffa14b9ae987995
GENG Minghong <58876849+GENGMinghong@users.noreply.github.com>
2021-10-26 14:35:49 +0800

    Create generate_train_test_txt.py

---
+++ classification/generate_train_test_txt.py
@@ -0,0 +1,42 @@
+import random
+import math
+
+"""
+Split the training data into training and validation data
+"""
+
+
+if __name__ == '__main__':
+    name = './labels/train_label.txt'
+    with open(name, 'r', encoding='utf-8') as f:
+        lines = f.readlines()  # 获取所有行
+        # sum = 0
+        file_names = [file_name for file_name in lines]
+        # for line in lines:  # 第i行
+        #     # 找到第一个空格
+        #     file_list.append(line)
+        #     # for j in range(len(line)):
+        #     #     if line[j].isspace() == True:
+        #     #         a = line[:j]
+        #     #         # if a not in list:
+        #     #         list.append(a)
+        #     #         sum += 1
+        f.close()
+
+    file_name_len = len(file_names)
+    random.Random(4).shuffle(file_names)
+
+    split_loc = math.floor(0.8 * file_name_len)
+
+    train_split = file_names[0:split_loc]
+    val_split = file_names[split_loc:]
+
+    with open('./labels/split_labels/train_split.txt', 'a', encoding='utf-8') as train_split_txt:
+        for train_img in train_split:
+            train_split_txt.write(train_img)
+        train_split_txt.close()
+
+    with open('./labels/split_labels/val_split.txt', 'a', encoding='utf-8') as val_split_txt:
+        for val_img in val_split:
+            val_split_txt.write(val_img)
+        val_split_txt.close()
