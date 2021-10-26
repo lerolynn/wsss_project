dca1184b9cf7922ffc8e759e58c3d78355684444
GENG Minghong <58876849+GENGMinghong@users.noreply.github.com>
2021-10-26 14:36:49 +0800

    Update LearnResNext.py

--- classification/LearnResNext.py
+++ classification/LearnResNext.py
@@ -21,10 +21,11 @@ from collections import OrderedDict
 from dataloader.DataLoader import my_Data_Set
 from dataloader.DataLoader import data_transforms
 from dataloader.DataLoader import Load_Image_Information_Train
-from dataloader.DataLoader import Load_Image_Information_Test
+from dataloader.DataLoader import Load_Image_Information_Val
 # from classification import Models
 from classification.Models import Resnext50
 from classification.Models import Resnext50_1024_2
+from classification.Models import Resnext50_1026_3
 from classification.Models import ResNet50
 
 from sklearn.metrics import precision_score
@@ -32,6 +33,7 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 
 import warnings
+
 warnings.filterwarnings("ignore")
 # -------------------------------------------   Parameters   -----------------------------------------------------------
 """
@@ -41,56 +43,57 @@ Check the setting each time when running this script!
 
 use_gpu = torch.cuda.is_available()
 evaluate = False
-batch_size = 32
+batch_size = 16
 # model_selected = "alexnet"
 # model_selected = "densenet"
 model_selected = "resnext"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-date = "1025"
-n_experiments = "exp_1"
+date = "1026"
+n_experiments = "exp_3"
 
 continue_training = False
 # -------------------------------------------   Data   -----------------------------------------------------------------
-train_Data = my_Data_Set(r'data/train_label.txt',
+train_Data = my_Data_Set(r'./labels/split_labels/train_split.txt',
                          transform=data_transforms['train'],
                          loader=Load_Image_Information_Train)
 # TODO: Use the normal way to load the validaton data. Don't do filp or anything on it.
-val_Data = my_Data_Set(r'data/val_label.txt',
+val_Data = my_Data_Set(r'./labels/split_labels/val_split.txt',
                        transform=data_transforms['val'],
-                       loader=Load_Image_Information_Test)
+                       loader=Load_Image_Information_Val)
 
-validation_split = 0.2
-shuffle_dataset = True
-random_seed = 42
-dataset_size = len(train_Data)
-indices = list(range(dataset_size))
-split = int(np.floor(validation_split * dataset_size))
-if shuffle_dataset:
-    np.random.seed(random_seed)
-    np.random.shuffle(indices)
-train_indices, val_indices = indices[split:], indices[:split]
-
+# validation_split = 0.2
+# shuffle_dataset = True
+# random_seed = 42
+# dataset_size = len(train_Data)
+# indices = list(range(dataset_size))
+# split = int(np.floor(validation_split * dataset_size))
+# if shuffle_dataset:
+#     np.random.seed(random_seed)
+#     np.random.shuffle(indices)
+# train_indices, val_indices = indices[split:], indices[:split]
+#
 # Dataloader
-train_sampler = SubsetRandomSampler(train_indices)
-valid_sampler = SubsetRandomSampler(val_indices)
-train_DataLoader = DataLoader(train_Data, batch_size=batch_size, sampler=train_sampler)
-validation_Dataloader = DataLoader(train_Data, batch_size=batch_size, sampler=valid_sampler)
+# train_sampler = SubsetRandomSampler(train_indices)
+# valid_sampler = SubsetRandomSampler(val_indices)
+# train_DataLoader = DataLoader(train_Data, batch_size=batch_size, sampler=train_sampler)
+# validation_Dataloader = DataLoader(train_Data, batch_size=batch_size, sampler=valid_sampler)
+
+train_DataLoader = DataLoader(train_Data, batch_size=batch_size)
+validation_Dataloader = DataLoader(val_Data, batch_size=batch_size)
 
 # TODO: Test the model on the public data
-val_DataLoader = DataLoader(val_Data, batch_size=1)
+# val_DataLoader = DataLoader(val_Data, batch_size=1)
 
 dataloaders = {'train': train_DataLoader,
-               'validation': validation_Dataloader,
-               'val': val_DataLoader}
+               'validation': validation_Dataloader}
 
 dataset_sizes = {'train': train_Data.__len__(),
-                 'validation': 0.2 * train_Data.__len__(),
-                 'val': val_Data.__len__()}
+                 'validation': val_Data.__len__()}
 
 # ------------------------------------------   Model   -----------------------------------------------------------------
 if model_selected == "resnext":
-    model = Resnext50(103)
+    model = Resnext50_1026_3(103)
     model = model.to(device)
 if model_selected == "resnet50":
     model = ResNet50(103)
@@ -115,15 +118,16 @@ def calculate_metrics(pred, target, threshold=0.5):
             'samples/f1': f1_score(y_true=target, y_pred=pred, average='samples'),
             }
 
+
 # -----------------------------------------   Training   ---------------------------------------------------------------
-max_epoch_number = 100
+max_epoch_number = 20
 learning_rate = 0.0001
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 # TODO: Check if this BCE loss is the correct one, as we are selecting sigmoid function to generate the output.
 criterion = nn.BCELoss()
 
-test_freq = 1000
-save_freq = 10  # save model at every 10 epochs
+test_freq = 500
+save_freq = 5  # save model at every 10 epochs
 
 # Training Loop
 epoch = 0
@@ -171,26 +175,40 @@ while True:
             loss_value_validate = np.mean(batch_validation_loss)
 
             result = calculate_metrics(np.array(model_result), np.array(targets))
-            print("epoch:{:2d} iter:{:3d} test: "
+            print("epoch:{:2d} iter:{:3d} val: "
                   "micro f1: {:.3f} "
                   "macro f1: {:.3f} "
                   "samples f1: {:.3f}".format(epoch, iteration,
                                               result['micro/f1'],
                                               result['macro/f1'],
                                               result['samples/f1']))
+            print("epoch:{:2d} iter:{:3d} val: "
+                  "micro precision: {:.3f} "
+                  "macro precision: {:.3f} "
+                  "samples precision: {:.3f}".format(epoch, iteration,
+                                                     result['micro/precision'],
+                                                     result['macro/precision'],
+                                                     result['samples/precision']))
+            print("epoch:{:2d} iter:{:3d} val: "
+                  "micro recall: {:.3f} "
+                  "macro recall: {:.3f} "
+                  "samples recall: {:.3f}".format(epoch, iteration,
+                                                  result['micro/recall'],
+                                                  result['macro/recall'],
+                                                  result['samples/recall']))
 
             print("epoch:{:2d} iter:{:3d} val: loss:{:.3f}".format(epoch, iteration, loss_value_validate))  # val loss
 
             model.train()
         iteration += 1
-    print("This epoch trained on {:2d} images.".format(iteration))
+    # print("This epoch trained on {:2d} images.".format(iteration))
     loss_value = np.mean(batch_losses)
 
     print("epoch:{:2d} iter:{:3d} train: loss:{:.3f}".format(epoch, iteration, loss_value))
-    if (epoch+1) % save_freq == 0:
+    if (epoch + 1) % save_freq == 0:
         # checkpoint_save(model, save_path, epoch)
-        torch.save(model.state_dict(), 'The_' + str(epoch) + '_epoch_ResNext' + date + n_experiments + '.pkl')
-        print("Save model:" + 'The_' + str(epoch) + '_epoch_ResNext' + date + n_experiments + '.pkl')
+        torch.save(model.state_dict(), 'The_' + str(epoch) + '_epoch_ResNext' + date + "_" + n_experiments + '.pkl')
+        print("Save model:" + 'The_' + str(epoch) + '_epoch_ResNext' + date + "_" + n_experiments + '.pkl')
     epoch += 1
     if max_epoch_number < epoch:
         break
